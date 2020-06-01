1b8cf1c79b75f0234370f17fe3196f3768cc1e85
Leandro Doctors <ldoctors@untref.edu.ar>
2020-05-31 23:57:00 -0300

    PASA: Debo poder crear un Nodo genérico vacío (lease: inválido)

--- src/ar/edu/untref/ayp2/Nodo.java
+++ src/ar/edu/untref/ayp2/Nodo.java
@@ -2,4 +2,14 @@ package ar.edu.untref.ayp2;
 
 public class Nodo<CualquierTipo> {
 
+	private CualquierTipo dato;
+
+	public CualquierTipo obtDato() {
+		return dato;
+	}
+
+	public boolean esValido() {
+		return (obtDato() != null);
+	}
+
 }
